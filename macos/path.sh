8393e516a6b926a89e3cd8d5c76580cbb437bd15
Ammon Lockwood <ammon@plaidtie.net>
2019-11-18 11:25:40 -0700

    :sparkles: Add Flutter
    Add stuff for Flutter development

--- macos/path.sh
+++ macos/path.sh
@@ -1,2 +1,12 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# Android SDK
+export ANDROID_HOME=/Users/$USER/Library/Android/sdk
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+# Flutter
+export PATH=/Users/$USER/Developer/flutter/bin:$PATH
+export PATH=/Users/$USER/Developer/flutter/.pub-cache/bin:$PATH
+export PATH=/Users/$USER/Developer/flutter/bin/cache/dart-sdk/bin:$PATH
+export PATH="$PATH":"$HOME/.pub-cache/bin"
